2e6c9912c0689f32af49a839c4d58a6661e3f9ab
ztctianna <ztctianna@163.com>
2017-12-17 19:17:41 +0800

    challenge6-2

--- news/app.py
+++ news/app.py
@@ -1,7 +1,25 @@
+from flask import abort,Flask,render_template
+import os
+import json
 
-class Files(obj):
+app = Flask(__name__)
 
+class Files(object):
+    def __init__(self):
+        self.result = self.get_file_content()
+    def get_file_content(self):
+        result = {}
+        for filename in os.listdir('/home/shiyanlou/files'):
+            file_path = os.path.join('/home/shiyanlou/files',filename)
+            with open(file_path) as f:
+                result[filename[:-5]] = json.load(f)
+        return result
 
+    def get_file_list(self):
+        return [item['title'] for item in self.result.values()]
+
+    def get_content(self,filename):
+        return self.result.get(filename)
 
 files = Files()
 
@@ -10,9 +28,9 @@ def index():
     title_list = files.get_file_list()
     return render_template('index.html',title_list = title_list)
 
-@app.route('files/<filename>')
+@app.route('/files/<filename>')
 def file(filename):
-    file_content = files.get_each_content(filename)
+    file_content = files.get_content(filename)
     if not file_content:
         abort(404)
     return render_template('file.html', file_item = file_content)
@@ -21,3 +39,7 @@ def file(filename):
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'),404
+
+
+if __name__ == '__main__':
+    app.run()
